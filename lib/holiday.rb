8c328863fad7b3a1d69b1a535a0ad53a128bf507
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-28 22:27:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash.each do |holiday, supply|
+  holiday_hash[:winter].each do |holiday, supply|
     if holiday == :christmas || holiday == :new_years
       << supply
     end
