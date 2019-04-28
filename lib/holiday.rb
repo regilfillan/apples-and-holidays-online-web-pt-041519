72c3ee5cb2b8a5027026d6bdef57fe7165579da6
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-28 23:12:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,7 @@ def all_holidays_with_bbq(holiday_hash)
     end
   end.flatten.compact
 end
-    # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+
 
 
 
